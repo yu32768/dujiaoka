380a7e38b6c1e64e998b9ba01d16a39fcd8d0d55
yupeng <yuzhiyu3@icloud.com>
2025-06-29 09:23:07 +0800

    Update footer

--- resources/views/unicorn/layouts/_footer.blade.php
+++ resources/views/unicorn/layouts/_footer.blade.php
@@ -3,7 +3,10 @@
     <div class="row">
         <div class="col-12 text-center">
             <div class="author">
-                <a href="https://store.shu315.com">@store.shu315.com</a>
+                Email: <a href="mailto:contact@shu315.com">contact@shu315.com</a>
+                <br>
+                <br>
+                <a href="https://store.shu315.com">store.shu315.com</a>
             </div>
             <div class="custom">
                 {!! dujiaoka_config_get('footer') !!}
